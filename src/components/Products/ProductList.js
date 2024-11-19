de5ce4c05478816613aeceb2bbb0536e63d118a5
smas0900 <saeedalqama@gmail.com>
2024-11-19 20:24:53 +0500

    add styles for product list

--- src/components/Products/ProductList.js
+++ src/components/Products/ProductList.js
@@ -83,12 +83,14 @@ const ProductList = ({ products, setProducts, setFilteredProducts }) => {
         ) : (
           // Display regular table headers when no items are selected
           <div className="table-headers">
-            <span>Product Name</span>
-            <span>Status</span>
-            <span>Quantity</span>
-            <span>Category</span>
-            <span>On Sale</span>
-            <span>Vendor</span>
+            <span></span>
+
+            <span className='pn'>Product Name</span>
+            <span className='pnstatus'>Status</span>
+            <span className='pnq'>Quantity</span>
+            <span className='pnc'>Category</span>
+            <span className='pns'>On Sale</span>
+            <span className='pnv'>Vendor</span>
           </div>
         )}
       </div>
@@ -99,6 +101,7 @@ const ProductList = ({ products, setProducts, setFilteredProducts }) => {
           {products.length > 0 ? (
             products.map((product) => (
               <tr key={product.id}>
+                <td></td>
                 <td>
                   <input
                     type="checkbox"
@@ -106,24 +109,26 @@ const ProductList = ({ products, setProducts, setFilteredProducts }) => {
                     onChange={() => handleCheckboxChange(product.id)}
                   />
                 </td>
+                {/* Image Column */}
                 <td>
-                  <div>
-                    <img src={product.image} alt={product.sku} style={{ width: '50px' }} />
-                  </div>
-                  <div>{product.name}</div>
+                  <img src={product.image} alt={product.sku} className="product-image" />
+                </td>
+                {/* Product Name Column */}
+                <td>
+                  <div className="product-n">{product.name}</div>
                   <div className="as">{product.sku}</div>
                   <div className="product-date">{product.date}</div>
                 </td>
-                <td>{product.status}</td>
-                <td>{product.qty}</td>
-                <td>{product.category}</td>
-                <td>{product.onSale ? 'Yes' : 'No'}</td>
-                <td>{product.vendor}</td>
+                <td className="product-al">{product.status}</td>
+                <td className="product-al">{product.qty}</td>
+                <td className="product-al">{product.category}</td>
+                <td className="product-al">{product.onSale ? 'Yes' : 'No'}</td>
+                <td className="product-al">{product.vendor}</td>
               </tr>
             ))
           ) : (
             <tr>
-              <td colSpan="7">No products found</td>
+              <td colSpan="9">No products found</td>
             </tr>
           )}
         </tbody>
